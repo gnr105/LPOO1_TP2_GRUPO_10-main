4cf820faab59067e51d8c74cfa0ec9c0fd23dbe6
pablomaldonado11 <maldonadonicolas43@gmail.com>
2026-05-15 01:06:14 -0300

    Add files via upload

---
+++ Ventas.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Vistas
+{
+    partial class Ventas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClienteVentas = new System.Windows.Forms.Label();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.cmbProductos = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dgvDetalle = new System.Windows.Forms.DataGridView();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelClienteVentas
+            // 
+            this.labelClienteVentas.AutoSize = true;
+            this.labelClienteVentas.Location = new System.Drawing.Point(42, 54);
+            this.labelClienteVentas.Name = "labelClienteVentas";
+            this.labelClienteVentas.Size = new System.Drawing.Size(95, 13);
+            this.labelClienteVentas.TabIndex = 0;
+            this.labelClienteVentas.Text = "Seleccione Cliente";
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(152, 51);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(121, 21);
+            this.cmbClientes.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Fecha de Venta";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(152, 105);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 3;
+            // 
+            // cmbProductos
+            // 
+            this.cmbProductos.FormattingEnabled = true;
+            this.cmbProductos.Location = new System.Drawing.Point(65, 33);
+            this.cmbProductos.Name = "cmbProductos";
+            this.cmbProductos.Size = new System.Drawing.Size(121, 21);
+            this.cmbProductos.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.cmbProductos);
+            this.groupBox1.Location = new System.Drawing.Point(60, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 174);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(83, 126);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(74, 82);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // dgvDetalle
+            // 
+            this.dgvDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetalle.Location = new System.Drawing.Point(78, 348);
+            this.dgvDetalle.Name = "dgvDetalle";
+            this.dgvDetalle.Size = new System.Drawing.Size(240, 150);
+            this.dgvDetalle.TabIndex = 6;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(361, 526);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(101, 23);
+            this.btnRegistrar.TabIndex = 7;
+            this.btnRegistrar.Text = "Registrar Ventas";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // Ventas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 571);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.dgvDetalle);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbClientes);
+            this.Controls.Add(this.labelClienteVentas);
+            this.Name = "Ventas";
+            this.Text = "Ventas";
+            this.Load += new System.EventHandler(this.Ventas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClienteVentas;
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.ComboBox cmbProductos;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.DataGridView dgvDetalle;
+        private System.Windows.Forms.Button btnRegistrar;
+    }
+}
---
+++ Ventas.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using ClaseBase;
+
+
+namespace Vistas
+{
+    public partial class Ventas : Form
+    {
+        // Usamos la ruta completa para forzar a Visual Studio a encontrar los adaptadores
+        ClaseBase.opticaDataSetTableAdapters.ClienteTableAdapter adpCli = new ClaseBase.opticaDataSetTableAdapters.ClienteTableAdapter();
+        ClaseBase.opticaDataSetTableAdapters.ProductoTableAdapter adpProd = new ClaseBase.opticaDataSetTableAdapters.ProductoTableAdapter();
+        ClaseBase.opticaDataSetTableAdapters.VentasTableAdapter adpVenta = new ClaseBase.opticaDataSetTableAdapters.VentasTableAdapter();
+        ClaseBase.opticaDataSetTableAdapters.Venta_DetalleTableAdapter adpDetalle = new ClaseBase.opticaDataSetTableAdapters.Venta_DetalleTableAdapter();
+
+        public Ventas()
+        {
+            InitializeComponent();
+        }
+
+        private void Ventas_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Llenamos los combos (Punto 7 del TP)
+                cmbClientes.DataSource = adpCli.GetData();
+                cmbClientes.DisplayMember = "Cli_Apellido";
+                cmbClientes.ValueMember = "Cli_DNI";
+
+                cmbProductos.DataSource = adpProd.GetData();
+                cmbProductos.DisplayMember = "Prod_Descripcion";
+                cmbProductos.ValueMember = "Prod_Codigo";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos: " + ex.Message);
+            }
+        }
+
+        // Botón para agregar productos a la grilla
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataRowView prod = (DataRowView)cmbProductos.SelectedItem;
+                decimal precio = Convert.ToDecimal(prod["Prod_Precio"]);
+                decimal cant = Convert.ToDecimal(txtCantidad.Text); // Usando txtCantidad
+                decimal total = precio * cant;
+
+                // Agregamos a la grilla: Código, Descripción, Precio, Cantidad y Total
+                dgvDetalle.Rows.Add(prod["Prod_Codigo"], prod["Prod_Descripcion"], precio, cant, total);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Por favor, ingresá un número válido en Cantidad.");
+            }
+        }
+
+        // Botón para guardar la Venta y el Detalle (Punto 7 completo)
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Reemplacé txtNroVenta por un número fijo o autoincremental si no tenés el cuadro
+                // Si tenés un cuadro para el número, cambialo por el nombre que le hayas puesto
+                int nroVenta = 1;
+                DateTime fecha = dtpFecha.Value;
+                string dni = cmbClientes.SelectedValue.ToString();
+
+                // 1. Guardar en tabla Venta
+                adpVenta.Insert(nroVenta, fecha, dni);
+
+                // 2. Guardar cada fila de la grilla en VentaDetalle
+                foreach (DataGridViewRow fila in dgvDetalle.Rows)
+                {
+                    if (fila.Cells[0].Value != null)
+                    {
+                        adpDetalle.Insert(
+                            nroVenta,
+                            Convert.ToInt32(fila.Cells[0].Value),
+                            Convert.ToDecimal(fila.Cells[2].Value),
+                            Convert.ToDecimal(fila.Cells[3].Value),
+                            Convert.ToDecimal(fila.Cells[4].Value)
+                        );
+                    }
+                }
+                MessageBox.Show("Venta registrada con éxito.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar: " + ex.Message);
+            }
+        }
+    }
+}
